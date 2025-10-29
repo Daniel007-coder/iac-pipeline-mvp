9930d91bb23a9b2fffe14572f3617604a5947ca1
Daniel Mkatakona <danimkata67@gmail.com>
2025-10-29 17:53:47 +0100

    Fix S3 ACL deprecation and added mock AWS credentials to CI workflow

--- modules/S3/main.tf
+++ modules/S3/main.tf
@@ -1,9 +1,13 @@
 resource "aws_s3_bucket" "example" {
   bucket = "my-iac-pipeline-demo-bucket"
-  acl    = "private"
 
   tags = {
     Name        = "DemoBucket"
     Environment = var.environment
   }
+}
+
+resource "aws_s3_bucket_acl" "example_acl" {
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
 }
